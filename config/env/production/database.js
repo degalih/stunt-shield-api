f848131cee1dae856eaa57a7e9035961edeab189
Galih Muhammad R <galihmramdan17@gmail.com>
2023-08-26 20:42:09 +0700

    bug: remove ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env("DATABASE_SSL"),
+      ssl: env.bool(true),
     },
   },
 });
