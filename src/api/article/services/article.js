9dfe1508ab1a8947b91dc9f19c1cfeb7fa74f5e4
Galih Muhammad R <galihmramdan17@gmail.com>
2023-08-31 16:56:07 +0700

    fix (close#33): migrate from railway to render

--- src/api/article/services/article.js
+++ src/api/article/services/article.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * recipe service
+ * article service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::recipe.recipe');
+module.exports = createCoreService('api::article.article');
--- src/api/poster/controllers/poster.js
+++ src/api/poster/controllers/poster.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * recipe controller
+ * poster controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::recipe.recipe');
+module.exports = createCoreController('api::poster.poster');
