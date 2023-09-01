04897d1095c97c6c3c5e24cb44a5c65c56df5859
Galih Muhammad R <galihmramdan17@gmail.com>
2023-09-01 10:40:24 +0700

    fix: cloudinary for production only

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,26 +9,4 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com", // cloudinary images
-            "market-assets.strapi.io",
-            "lh3.googleusercontent.com", // google avatars
-            "platform-lookaside.fbsbx.com", // facebook avatars
-          ],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
 ];
